c5b3887b3481a1e7ab2a58804df9cff6ca9d2ec4
Kimd0ng <kdongh0406@gmail.com>
2026-03-24 16:26:45 +0900

    feat: 03_Vue 06_단일파일컴포넌트 advanced 실습

---
+++ 03_Vue/02_advanced/06_단일파일컴포넌트/todolist/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <TodoListItem
+      v-for="todoitem in todolist"
+      :key="todoitem.id"
+      :todoitem="todoitem"
+      @toggle="toggleCompleted"
+      @delete="deleteTodo"
+    ></TodoListItem>
+  </ul>
+</template>
+
+<script setup>
+import TodoListItem from './TodoListItem.vue';
+
+const emit = defineEmits(['toggle', 'delete']);
+
+const toggleCompleted = (id) => {
+  emit('toggle', id);
+};
+
+const deleteTodo = (id) => {
+  emit('delete', id);
+};
+
+const props = defineProps({
+  todolist: Array,
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 03_Vue/02_advanced/06_단일파일컴포넌트/todolist/src/components/TodoListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li @click="toggleCompleted(todoitem.id)">
+    <span
+      :style="{ textDecoration: todoitem.completed ? 'line-through' : 'none' }"
+    >
+      {{ todoitem.todo }} {{ todoitem.completed ? '[완료]' : '' }}
+    </span>
+
+    <!-- 삭제하기 -->
+    <button @click="deleteTodo(todoitem.id)">삭제</button>
+  </li>
+</template>
+
+<script setup>
+const emit = defineEmits(['toggle', 'delete']);
+
+const toggleCompleted = (id) => {
+  emit('toggle', id);
+};
+const deleteTodo = (id) => {
+  emit('delete', id);
+};
+
+const props = defineProps({
+  todoitem: Object,
+});
+</script>
+
+<style lang="scss" scoped></style>
